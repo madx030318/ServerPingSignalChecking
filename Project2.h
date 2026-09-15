6bd6ac7742eba5814a2039d1b83ac00538f51c82
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-16 01:14:49 +0500

    Update Project2.h

--- Project2.h
+++ Project2.h
@@ -1,1 +1,19 @@
+class Backup {
+private:
 
+Signal* backupHead;
+Signal* backuptrail;
+
+public:
+Backup() {
+  backupHead = nullptr;
+  backuptrail = nullptr;
+}
+
+void createBackup(Signal* originalHead, Signal* originalLength);
+void printInfo();
+
+~Backup();
+
+
+}
