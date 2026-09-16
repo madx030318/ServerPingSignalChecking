5a0a546629916168b47adcb86d99a21b2cf8cc83
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-16 21:10:46 +0500

    Update Project2.cpp
    Adding the backup constructor whoch starts initialize the backup

--- Project2.cpp
+++ Project2.cpp
@@ -45,14 +45,58 @@ void Backup::AddPing(int startping, string date, string status)
 
 void Backup::CreateBackup(Signal* originalHead, int originalLength) {
     Signal* current = originalHead;
-	while (current == nullptr)
+	while (current != nullptr)
 		{
 			AddPing(
 				current->startping,
 				current->date,
 				current->status
-			)
+			);
+
+			current = current->nextping;
 		}
+
+	cout << "Backup created successfully." << endl;
+    cout << "Backup length: " << length << endl;
+
+	
+}
+
+void Backup::printInfo() {
+	Signal* current = backupHead;
+	while (current != nullptr)
+    {
+        cout << "Ping: " << current->startping << endl;
+        cout << "Date: " << current->date << endl;
+        cout << "Status: " << current->status << endl;
+        cout << "Backup: " << current->bIsBackup << endl;
+
+        cout << "-------------------" << endl;
+
+        current = current->nextping;
+    }
+
+    cout << "Total backup signals: " << length << endl;
+}
+	
+}
+
+Backup::~Backup() {
+	
+	Signal* current = backupHead;
+
+    while (current != nullptr)
+    {
+        Signal* temp = current;
+
+        current = current->nextping;
+
+        delete temp;
+    }
+
+    backupHead = nullptr;
+    backupTail = nullptr;
+}
 }
 
 
