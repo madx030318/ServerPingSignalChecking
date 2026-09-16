1513b0f1b1695eb0217d42daf4c4d789e9ffae73
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-16 13:40:54 +0500

    Update Project2.cpp
    Changes within project file cpp as calling functions inside file

--- Project2.cpp
+++ Project2.cpp
@@ -5,77 +5,22 @@
 using namespace std;
 
 class Signal {
-	public:
-		int startping;
-        string date;
-        string status;
-        bool bIsBackup;
-		Signal* nextping;
-		
-		Signal(int startping) {
+		Signal(int startping, string date, bool status, bool bIsBackup) {
 			this->startping = startping;
 			this->date = date;
 			this->status = status;
-			bIsBackup = false;
+			this->bIsBackup = false;
 			
-			nextping = nullptr;
+			this->nextping = nullptr;
 		}
 };
 
-class Generation {
-	private:
-		Signal* head;
-		Signal* tail;
-		int length;
-	
-	public:
-		Generation(int startping, string date, string status) {
-		
-		Signal* newCode = new Signal(startping, date, status);
-		head = newCode;
-		tail = newCode;
-		length = 24;
-		
-	}
-	
-		
-	}
-	
-	void printList() {
-		Signal* temp = head;
-		if (temp != nullptr) {
-			cout << temp->ping << endl;
-			cout << texmp->next;
-			cout << "The Server cycle is over. System works correctly ";
-		}
-	}
-	
-	void checktheCycleCorrectness(int startping) {
-	if (length == 24 && newCode->status = "ON") {
-        cout << "No interruptions throughout the ping process." << endl;
-    }
-    else {
-		cout << "There is interruption within ping cycle." << endl;
-        cout << "There is interruption within ping cycle. System is rebooting. Ping is stopped." << endl;
-    }
-	}
+Backup::Backup() {
 
-    void DoesBackupWasCreated() {
-		if (head !== nullptr && head->status == "ON") {
-			cout << "Server discovered." << endl;
-			cout << "Server is active." << endl;
-		}
+	backupHead = nullptr;
+    backupTail = nullptr;
+    length = 0;
 
-		else {
-			cout << "Server is disabled" << endl;
-			head->bIsBackup= true;
-			cout << "Creating server backup..." << endl;
-			cout << "Backup created successfully." << endl;
-		}
-		
-	}
+}
 
-	}
-	 
-	
-};
+
