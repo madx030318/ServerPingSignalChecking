441fed89750b759579e216a8970ced9b9b428f2d
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-16 13:20:58 +0500

    Update Project2.h
    In header file defined the functions for server ping answering

--- Project2.h
+++ Project2.h
@@ -3,15 +3,22 @@ private:
 
 Signal* backupHead;
 Signal* backuptrail;
+int length;
 
 public:
 Backup() {
   backupHead = nullptr;
   backuptrail = nullptr;
+  int length = 24;
 }
+
+Server(int startping);
+
+~Server();
 
 void createBackup(Signal* originalHead, Signal* originalLength);
 void printInfo();
+void AddPing(int startping);
 
 ~Backup();
 
